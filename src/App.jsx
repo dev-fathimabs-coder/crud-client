e99072c0b4823ea528149c736f963e2cafb393ef
dev-fathimabs-coder <devfathimabs@gamil.com>
2026-01-04 18:46:11 +0530

    changes in somefiles

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,57 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-     <div>
-       <form action="">
-        UserName
-        <input type="text" name='username' />
-        Email
-        <input type="email" name='email' />
-        Password
-        <input type="password"  name='password'/>
-        <button onClick={{}}>Sign Up / Login</button>
+  let [username, setUserNAme] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  //PREVENT PAGE REFRESH
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let data = { username, email, password }
+    console.log(data);
+    
+
+    fetch("http://localhost:5000/users", {
+      method: 'POST',
+      body: JSON.stringify(data)
+
+    }).then(res => {
+      res.json()
+    })
+      .then((result) => {
+        console.log("Response from server:", result);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      })
+  }
+
+return (
+  <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>UserName</label>
+        <input type="text"
+          value={username}
+          onChange={(e) => { setUserNAme(e.target.value) }} />
+        <label>Email</label>
+        <input type="email"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value) }} />
+        <label>Password</label>
+        <input type="password"
+          value={password}
+          onChange={(e) => { setPassword(e.target.value) }} />
+
+        <button>Sign Up</button>
       </form>
-     </div>
-    </>
-  )
+    </div>
+  </>
+)
 }
 
-export default App
+export default App;
